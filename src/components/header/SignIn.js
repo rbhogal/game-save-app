d7605d6f332908d0dd8f460430def9772d4039bd
rohitb <rbhogal23@gmail.com>
2021-06-22 12:09:21 -0700

    twitch sign in styling

--- src/components/header/SignIn.js
+++ src/components/header/SignIn.js
@@ -9,10 +9,11 @@ const SignIn = () => {
       <button className="drop-btn">
         Sign In &nbsp; <ion-icon name="chevron-down-outline"></ion-icon>
       </button>
-      <div className="dropdown-content">
+      <div className="dropdown-content dropdown-content-sign-in">
         <Link>
-          {/* <button className="sign-in-btn">Sign In</button>
-          <button className="sign-in-btn">Create Account</button> */}
+          <button className="sign-in-btn">
+            Twitch <ion-icon name="logo-twitch"></ion-icon>
+          </button>
         </Link>
       </div>
     </div>
