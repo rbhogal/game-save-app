6692e83ba3c2c1d86d910363332be3fc3b929a14
rohitb <rbhogal23@gmail.com>
2021-06-22 16:58:40 -0700

    twitch verification redirect added

--- src/components/header/SignIn.js
+++ src/components/header/SignIn.js
@@ -2,18 +2,20 @@ import React from "react";
 
 import "./SignIn.css";
 import { Link } from "react-router-dom";
+import TwitchAuth from "../TwitchAuth";
 
 const SignIn = () => {
+
+
+
   return (
     <div className="dropdown">
       <button className="drop-btn">
         Sign In &nbsp; <ion-icon name="chevron-down-outline"></ion-icon>
       </button>
       <div className="dropdown-content dropdown-content-sign-in">
-        <Link>
-          <button className="sign-in-btn">
-            Twitch <ion-icon name="logo-twitch"></ion-icon>
-          </button>
+        <Link >
+          <TwitchAuth />
         </Link>
       </div>
     </div>
