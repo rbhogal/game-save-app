77d57138d39fce36e25d8f0b057501f99c2ec899
rohitb <rbhogal23@gmail.com>
2021-06-21 18:08:14 -0700

    centered header styling

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -9,26 +9,10 @@ const Header = () => {
   return (
     <nav>
       <header>
-        <ion-icon className="logo" name="game-controller-outline"></ion-icon>
-        <div className="dropdown">
-          <button className="drop-btn">
-            ALL GAMES &nbsp; <ion-icon name="chevron-down-outline"></ion-icon>
-          </button>
-          <div className="dropdown-content">
-            <Link>Shooter</Link>
-            <Link>Platform</Link>
-            <Link>Fighting</Link>
-            <Link>Puzzle</Link>
-            <Link>Racing</Link>
-            <Link>Real Time Strategy</Link>
-            <Link>Role Playing Game</Link>
-            <Link>Sport</Link>
-            <Link>Strategy</Link>
-            <Link>Adventure</Link>
-            <Link>Arcade</Link>
-            <Link>Indie</Link>
-            <Link>MOBA</Link>
-          </div>
+        <div className="logo-icon">
+          <Link to="/">
+            <ion-icon name="game-controller-outline"></ion-icon>
+          </Link>
         </div>
         <SearchBox />
         <SignIn />
--- src/components/header/SignIn.js
+++ src/components/header/SignIn.js
@@ -1,10 +1,21 @@
 import React from "react";
 
+import "./SignIn.css";
+import { Link } from "react-router-dom";
+
 const SignIn = () => {
   return (
-    <button className="drop-btn">
-      Sign In &nbsp; <ion-icon name="chevron-down-outline"></ion-icon>
-    </button>
+    <div className="dropdown">
+      <button className="drop-btn">
+        Sign In &nbsp; <ion-icon name="chevron-down-outline"></ion-icon>
+      </button>
+      <div className="dropdown-content">
+        <Link>
+          {/* <button className="sign-in-btn">Sign In</button>
+          <button className="sign-in-btn">Create Account</button> */}
+        </Link>
+      </div>
+    </div>
   );
 };
 
