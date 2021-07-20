94696a47b5e7bf466e150158819cf86197132e9d
rohitb <rbhogal23@gmail.com>
2021-07-20 12:21:10 -0700

    all search results work

--- src/components/games/GameList.js
+++ src/components/games/GameList.js
@@ -5,26 +5,18 @@ import axios from 'axios';
 import { selectAppToken } from '../../features/admin/appTokenSlice';
 import GamesMultipleRowsScroll from './GamesMultipleRowsScroll';
 import './GameList.css';
-import { getSearchAsync } from '../../features/users/userSlice';
 import AuthContext from '../../store/auth-context';
 
 const GameList = () => {
   const [games, setGames] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const token = useSelector(selectAppToken);
   const dispatch = useDispatch();
   const [search, setSearch] = useState('');
-  // const search = useSelector(state => state.user.search);
-  // const [newSearch, setNewSearch] = useState('');
-  // const [prevSearch, setPrevSearch] = useState('');
+
   const authCtx = useContext(AuthContext);
   const newSearch = authCtx.search;
-  console.log(`athCtx search = ${newSearch}`);
   // console.log(`Retrieving search: '${search}', from state`);
-
-  // useEffect(() => {
-  //   dispatch(getSearchAsync());
-  // }, [dispatch]);
 
   useEffect(() => {
     setSearch(newSearch);
@@ -42,7 +34,7 @@ const GameList = () => {
         'Client-ID': process.env.REACT_APP_CLIENT_ID,
         Authorization: `Bearer ${token}`,
       },
-      data: `search"${search}"; fields summary, cover.image_id, genres.name, name, total_rating; where platforms = (6, 48, 49, 130); limit 48;`,
+      data: `search"${search}"; fields summary, cover.image_id, genres.name, name, total_rating; where platforms = (6, 48, 49, 130) & genres.name != null & cover.image_id != null; limit 48;`,
     })
       .then(resp => {
         setGames(resp.data);
@@ -53,43 +45,16 @@ const GameList = () => {
         console.log(err);
       });
   };
-  /* 
-  useEffect(() => {
-    const checkNewSearch = () => {
-      const newSearch = localStorage.getItem('search');
-      if (newSearch) {
-        setSearch(newSearch);
-        console.log(`new search: ${newSearch}`);
-      } else {
-        console.log('did not set search');
-      }
-    };
-
-    window.addEventListener('storage', checkNewSearch());
-
-    return () => {
-      window.removeEventListener('storage', checkNewSearch());
-    };
-    // dispatch(getSearchAsync());
-
-    // console.log(`4) getting search from database...`);
-    // console.log(`Search from firebase ${search}`);
-
-    // if (token ) {
-    //   dispatch(getSearchAsync());
-    //   console.log(`4) getting search from database...`);
-    // }
-  }, []);
- */
 
   useEffect(() => {
     if (token && search) searchGames();
-    setIsLoading(false);
+    // setIsLoading(false);
   }, [token, search]);
 
   return (
     <div className="GameList">
       {!isLoading && <GamesMultipleRowsScroll games={games} />}
+      {/* {isLoading && <p>Loading...</p>} */}
     </div>
   );
 };
--- src/components/games/GamesMultipleRowsScroll.js
+++ src/components/games/GamesMultipleRowsScroll.js
@@ -1,28 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Slider from 'react-slick';
+import _ from 'lodash';
 
 import './GamesHorizontalScroll.css';
 import { Link } from 'react-router-dom';
 
 const GamesMultipleRowsScroll = props => {
   const [bookmark, setBookmark] = useState(false);
+  const [filteredGames, setFilteredGames] = useState([]);
 
-  const name = (params) => {
-      // basically give a new array by checking if current array's items even exit
-      // (game.genres[0].name) ? then add it if not then N/A 
-  }
-  
+  //   const filterData = () => {
+  //     /*
+  //     Get incoming array from props.games
+  //     Check if a name, image_id, summary, genres[0].name and total.rating exist
+  //     If they don't exist make that value N/A
+  //     Make a new array with adjusted values
 
+  //     Methods needed: map, and
+  //     */
+
+  //     // basically give a new array by checking if current array's items even exit
+  //     // (game.genres[0].name) ? then add it if not then N/A
+
+  //     const filteredGames = props.games.filter(game => !game.genres[0].name);
+  //     setFilteredGames(filteredGames);
+  //   };
+
+  //   useEffect(() => {
+  //     filterData();
+  //   }, [filteredGames]);
 
   const settings = {
     className: 'center',
-    centerMode: false,
-    infinite: false,
+    dots: true,
+    // centerMode: false,
+    infinite: true,
     // centerPadding: '60px',
     slidesToShow: 8,
+    slidesToScroll: 8,
     speed: 500,
     rows: 1,
-    slidesPerRow: 3,
+    slidesPerRow: 1,
     className: 'slick-arrows',
   };
 
@@ -30,45 +48,53 @@ const GamesMultipleRowsScroll = props => {
     setBookmark(!bookmark);
   };
 
-  return (
-    <>
-      <Slider {...settings}>
-        {props.games.map(game => (
-          <>
-            <Link to={`/gamelist/games/${game.name}`}>
-              <div className="game-card">
-                <img
-                  src={`//images.igdb.com/igdb/image/upload/t_cover_big/${game.cover.image_id}.jpg`}
-                  alt={game.name}
-                ></img>
+  const showSearchResults = () => {
+    if (!_.isEmpty(props.games)) {
+      return (
+        <>
+          <Slider {...settings}>
+            {props.games.map(game => (
+              <>
+                <Link to={`/gamelist/games/${game.name}`}>
+                  <div className="game-card">
+                    <img
+                      src={`//images.igdb.com/igdb/image/upload/t_cover_big/${game.cover.image_id}.jpg`}
+                      alt={game.name}
+                    ></img>
 
-                <div className="game-card-summary-box">
-                  <p className="game-card-summary">{game.summary}</p>
-                </div>
+                    <div className="game-card-summary-box">
+                      <p className="game-card-summary">{game.summary}</p>
+                    </div>
 
-                <div className="game-card-content">
-                  <div className="game-card-genre-box">
-                    <p className="game-card-genre">{`${game.genres[0].name}`}</p>
+                    <div className="game-card-content">
+                      <div className="game-card-genre-box">
+                        <p className="game-card-genre">{`${game.genres[0].name}`}</p>
+                      </div>
+                      <Link onClick={handleBookmarkClick}>
+                        <ion-icon
+                          className="game-card-bookmark"
+                          name={bookmark ? 'bookmark' : 'bookmark-outline'}
+                        ></ion-icon>
+                      </Link>
+                      <div className="game-card-rating-box">
+                        <p className="game-card-rating">
+                          {Math.round(game.total_rating)}
+                        </p>
+                      </div>
+                    </div>
                   </div>
-                  <Link onClick={handleBookmarkClick}>
-                    <ion-icon
-                      className="game-card-bookmark"
-                      name={bookmark ? 'bookmark' : 'bookmark-outline'}
-                    ></ion-icon>
-                  </Link>
-                  <div className="game-card-rating-box">
-                    <p className="game-card-rating">
-                      {Math.round(game.total_rating)}
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </Link>
-          </>
-        ))}
-      </Slider>
-    </>
-  );
+                </Link>
+              </>
+            ))}
+          </Slider>
+        </>
+      );
+    }
+
+
+  };
+
+  return <>{showSearchResults()}</>;
 };
 
 export default GamesMultipleRowsScroll;
