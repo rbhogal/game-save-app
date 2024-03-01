25894ee0c6e3d8bee3ea71ea67ef290eb2d0a20b
Rohit Bhogal <rohit@Rohits-MacBook-Pro.local>
2024-02-29 17:32:14 -0800

    added active arrow select project functionality. Refine solution later

--- src/components/RepairPage.js
+++ src/components/RepairPage.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './RepairPage.css';
 
 const RepairPage = () => {
+  // TODO: Come up with a more elegant solution
+  const [activeLink1, setActiveLink1] = useState(true);
+  const [activeLink2, setActiveLink2] = useState(null);
+
   return (
     <div className="repairPage">
       <div className="headingContainer">
@@ -12,7 +16,13 @@ const RepairPage = () => {
       <div className="subHeadingContainer">
         <p>Meanwhile learn more about this project here:</p>
         <ul className="projectLinks">
-          <li>
+          <li
+            onMouseEnter={() => {
+              setActiveLink1(true);
+              setActiveLink2(false);
+            }}
+            className={`link ${activeLink1 ? 'active' : ''}`}
+          >
             <a
               href="https://rbhogal.github.io/project-game-save.html"
               target="_blank"
@@ -21,7 +31,13 @@ const RepairPage = () => {
               https://rbhogal.github.io/project-game-save.html
             </a>
           </li>
-          <li>
+          <li
+            onMouseEnter={() => {
+              setActiveLink2(true);
+              setActiveLink1(false);
+            }}
+            className={`link ${activeLink2 ? 'active' : ''}`}
+          >
             <a
               href=" https://github.com/rbhogal/game-save-app"
               target="_blank"
