6bf19f00cd083d5df3e5dd73e1c901f48ef6afce
Rohit Bhogal <rohit@Rohits-MacBook-Pro-2.local>
2024-03-03 09:51:59 -0800

    add hover logic over links

--- src/components/RepairPage.js
+++ src/components/RepairPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './RepairPage.css';
 
@@ -6,6 +6,26 @@ const RepairPage = () => {
   // TODO: Come up with a more elegant solution
   const [activeLink1, setActiveLink1] = useState(true);
   const [activeLink2, setActiveLink2] = useState(null);
+
+  useEffect(() => {
+    const eventListener = e => {
+      console.log({ e });
+
+      if (activeLink1) {
+        setActiveLink1(false);
+        setActiveLink2(true);
+      }
+      if (activeLink2) {
+        setActiveLink2(false);
+        setActiveLink1(true);
+      }
+    };
+
+    window.addEventListener('keydown', eventListener);
+    return () => {
+      window.removeEventListener('keydown', eventListener);
+    };
+  }, [activeLink1, activeLink2]);
 
   return (
     <div className="repairPage">
